1c8c29f7f784ff40ae7bf1d97c563968eda8115b
mantaslv <mantasvolkas@gmail.com>
2024-07-15 15:41:38 +0100

    Lists numbers up to 15

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,6 +1,7 @@
 object FizzBuzz {
   def generate(number: Int): String = {
     (1 to number).toList.map {
+      case x if x % 5 == 0 && x % 3 == 0 => "FizzBuzz"
       case x if x % 5 == 0 => "Buzz"
       case x if x % 3 == 0 => "Fizz"
       case x => x.toString
--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -19,4 +19,8 @@ class FizzBuzzTest extends AnyFunSuite {
   test("lists the numbers up to five") {
     assert(FizzBuzz.generate(5) == "1, 2, Fizz, 4, Buzz")
   }
+
+  test("lists the numbers up to fifteen") {
+    assert(FizzBuzz.generate(15) == "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz")
+  }
 }
