c3ed980786e87f1f56bb3792becc5b960bdcd18b
mantaslv <mantasvolkas@gmail.com>
2024-07-15 15:35:21 +0100

    Implemented list and fizz condition

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,11 +1,8 @@
 object FizzBuzz {
   def generate(number: Int): String = {
-    if (number == 1) {
-      "1"
-    } else if (number == 2) {
-      "1, 2"
-    } else {
-      "1, 2, Fizz"
-    }
+    (1 to number).toList.map {
+      case x if x % 3 == 0 => "Fizz"
+      case x => x.toString
+    }.mkString(", ")
   }
 }
