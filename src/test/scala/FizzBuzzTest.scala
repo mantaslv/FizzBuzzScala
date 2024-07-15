ba5f7de147ed22590fb3d827c4c3d82d1f5fa0a0
mantaslv <mantasvolkas@gmail.com>
2024-07-15 14:36:10 +0100

    Fixed errors

--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -1,14 +1,18 @@
 import org.scalatest.funsuite.AnyFunSuite
 class FizzBuzzTest extends AnyFunSuite {
   test("lists the numbers up to one") {
-    assert(FizzBuzz.generate(1) === "1")
+    assert(FizzBuzz.generate(1) == "1")
   }
 
   test("lists the numbers up to two") {
-    assert(FizzBuzz.generate(2) === "1, 2")
+    assert(FizzBuzz.generate(2) == "1, 2")
   }
 
   test("lists the numbers up to three") {
-    assert(FizzBuzz.generate(3) === "1, 2, Fizz")
+    assert(FizzBuzz.generate(3) == "1, 2, Fizz")
+  }
+
+  test("lists the numbers up to four") {
+    assert(FizzBuzz.generate(4) == "1, 2, Fizz, 4")
   }
 }
