cc9ce6a44773ddc8fd6ef0d72c8522b04c55b64b
mantaslv <mantasvolkas@gmail.com>
2024-07-16 11:08:55 +0100

    Added partial execution functions

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,8 +1,15 @@
 object FizzBuzz {
-  private def nToFizzBuzzValue(number: Int): String = (number % 5, number % 3) match { // tuple pattern matching reduces conditionals
-    case (0, 0) => "FizzBuzz"
-    case (0, _) => "Buzz"
-    case (_, 0) => "Fizz"
+  private def isDivisibleBy(dividend: Int, divisor: Int): Boolean = {
+    dividend % divisor == 0
+  }
+
+  private val isDivisibleBy5 = isDivisibleBy(_, 5)
+  private val isDivisibleBy3 = isDivisibleBy(_, 3)
+
+  private def nToFizzBuzzValue(number: Int): String = (isDivisibleBy5(number), isDivisibleBy3(number)) match { // tuple pattern matching reduces conditionals
+    case (true, true) => "FizzBuzz"
+    case (true, _) => "Buzz"
+    case (_, true) => "Fizz"
     case _      => number.toString
   }
 
