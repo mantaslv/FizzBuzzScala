8d351e7fe606f80e1c87392ebf059d67b3cb4876
mantaslv <mantasvolkas@gmail.com>
2024-07-16 09:12:04 +0100

    Refe q actored nToFizzBuzzValue into own function

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,10 +1,14 @@
 object FizzBuzz {
+  def nToFizzBuzzValue(number: Int): String = number match {
+    case x if x % 15 == 0 => "FizzBuzz"
+    case x if x % 5 == 0 => "Buzz"
+    case x if x % 3 == 0 => "Fizz"
+    case x => x.toString
+  }
+
   def generate(number: Int): String = {
-    (1 to number).toList.map {
-      case x if x % 5 == 0 && x % 3 == 0 => "FizzBuzz"
-      case x if x % 5 == 0 => "Buzz"
-      case x if x % 3 == 0 => "Fizz"
-      case x => x.toString
-    }.mkString(", ")
+    (1  to number).toList
+      .map(nToFizzBuzzValue)
+      .mkString(", ")
   }
 }
