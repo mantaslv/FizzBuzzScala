bb55216946f8235f6402a99d26b538e75ff7c5d2
mantaslv <mantasvolkas@gmail.com>
2024-07-16 14:52:09 +0100

    Added recursion and curried solutions

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -3,19 +3,36 @@ object FizzBuzz {
     dividend % divisor == 0
   }
 
-  private val isDivisibleBy5 = isDivisibleBy(_, 5)
+  private def curriedIsDivisibleBy(divisor: Int): Int => Boolean = {
+    (dividend: Int) => dividend % divisor == 0
+  }
+
+  private val isDivisibleBy5 =
+    // isDivisibleBy(_, 5)
+    curriedIsDivisibleBy(5)
+
   private val isDivisibleBy3 = isDivisibleBy(_, 3)
 
-  private def nToFizzBuzzValue(number: Int): String = (isDivisibleBy5(number), isDivisibleBy3(number)) match { // tuple pattern matching reduces conditionals
+  private def nToFizzBuzzValue(number: Int): String = (isDivisibleBy5(number), isDivisibleBy3(number)) match {
     case (true, true) => "FizzBuzz"
     case (true, _) => "Buzz"
     case (_, true) => "Fizz"
     case _      => number.toString
   }
 
+  private def recursiveFn(number: Int): String = {
+    if (number < 1) ""
+    else {
+      val previous = recursiveFn(number - 1)
+      val current = nToFizzBuzzValue(number)
+      if (previous == "") current else previous + ", " + current
+    }
+  }
+
   def generate(number: Int): String = {
-    (1  to number) // .toList not required as range provides map function directly
+    /*(1  to number) // .toList not required as range provides map function directly
       .map(nToFizzBuzzValue)
-      .mkString(", ")
+      .mkString(", ")*/
+    recursiveFn(number)
   }
 }
